93931a2a70101a3fa8f695a81baa87ba4ef5aee8
Ryan RyanBergquist Bergquist <r.bergquist7@gmail.com>
2015-08-18 17:04:08 -0700

    setting up login screen. Need to add functionality of login screen

---
+++ app/src/main/java/com/lawnservice/ryanbergquist/mowmylawn/Post_New_Lawn.java
@@ -0,0 +1,7 @@
+package com.lawnservice.ryanbergquist.mowmylawn;
+
+/**
+ * Created by ryanbergquist on 8/18/15.
+ */
+public class Post_New_Lawn {
+}
---
+++ app/src/main/java/com/lawnservice/ryanbergquist/mowmylawn/Review_Applicants.java
@@ -0,0 +1,8 @@
+package com.lawnservice.ryanbergquist.mowmylawn;
+
+/**
+ * Created by ryanbergquist on 8/18/15.
+ */
+public class Review_Applicants {
+
+}
